884fb9ce70b041c74c27a646f8ee09ad49c6dfbd
Juan Gonon <juanfegonon27@gmail.com>
2025-01-06 10:54:08 -0600

    pantalla publica - mostar el trabajo acutal

--- public/js/public.js
+++ public/js/public.js
@@ -1,2 +1,26 @@
 
-console.log('Público HTML')
+function renderTickets (tickets = []) {
+  for (let i = 0; i < tickets.length; i++) {
+    if (i >= 4) break
+
+    const ticket = tickets[i]
+
+    if (!ticket) continue
+
+    const lblTicket = document.querySelector(`#lbl-ticket-0${i + 1}`)
+    const lblDesk = document.querySelector(`#lbl-desk-0${i + 1}`)
+
+    lblTicket.innerHTML = `Ticket ${ticket.number}`
+    lblDesk.innerHTML = ticket.handleAtDesk
+  }
+}
+
+async function loadCurrentTickets (params) {
+  const tickets = await fetch('/api/ticket/working-on').then((res) => res.json())
+
+  renderTickets(tickets)
+
+//   console.log(tickets)
+}
+
+loadCurrentTickets()
