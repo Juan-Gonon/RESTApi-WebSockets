4d522e750aca8d4780429d56b451580eb4252689
Juan Gonon <juanfegonon27@gmail.com>
2025-01-05 20:08:04 -0600

    details pantalla desk

--- public/js/desk.js
+++ public/js/desk.js
@@ -1,10 +1,31 @@
 const lblPending = document.getElementById('lbl-pending')
+const deskHeader = document.querySelector('h1')
+
+const searchParams = new URLSearchParams(window.location.search)
+const noMoreAlert = document.querySelector('.alert')
+
+if (!searchParams.has('escritorio')) {
+  window.location = 'index.html'
+  throw new Error('Escritorio es requerido')
+}
+
+function checkTicketCount (currentCount = 0) {
+  // noMoreAlert.classList.toggle('d-none')
+  if (currentCount === 0) {
+    noMoreAlert.classList.remove('d-none')
+  } else {
+    noMoreAlert.classList.add('d-none')
+  }
+  lblPending.innerHTML = currentCount
+}
 
 async function loadInitialCount () {
-  const pending = await fetch('/api/ticket').then((resp) => resp.json())
-
-  lblPending.innerHTML = pending.length || 0
+  const pendingTickets = await fetch('/api/ticket').then((resp) => resp.json())
+  checkTicketCount(pendingTickets.length)
 }
+
+const deskNumber = searchParams.get('escritorio')
+deskHeader.innerText = deskNumber
 
 function connectToWebSockets () {
   // eslint-disable-next-line no-undef
@@ -14,7 +35,8 @@ function connectToWebSockets () {
     //  console.log(event.data) // onTicket-cont-changed
     const { type, payload } = JSON.parse(event.data)
     if (type !== 'on-ticket-count-changed') return
-    lblPending.innerHTML = payload
+    // lblPending.innerHTML = payload
+    checkTicketCount(payload)
   }
 
   socket.onclose = (event) => {
